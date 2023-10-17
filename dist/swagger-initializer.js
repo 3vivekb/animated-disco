ea85884994767e4ad182b310f4c9771c395c087d
Vivek Bansal <3vivekb@gmail.com>
2023-10-16 23:43:24 -0700

    Add the swagger ui.

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    // url: "openapi.json",
+    url: "http://127.0.0.1:5000/openapi.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
